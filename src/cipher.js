fe57c95e41b4ddf67086ae84f93612e8e8d1583b
Erika Roque <roquerika@gmail.com>
2023-08-03 13:01:13 -0400

    Agregar readme

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,73 @@
-const cipher = {
-  // ...
-};
 
+const cipher = {
+
+  encode: (desplazamiento, mensaje) => {
+    let mensajeEncriptado = "";
+    let nuevaLetraCodificada = "";
+
+    if (typeof Number(desplazamiento) !== 'number' || typeof mensaje !== 'string') {
+      throw new TypeError('Agrumentos inválidos');
+    }
+    // typeof Number(desplazamiento) !== 'number': Aquí se convierte la variable deplazamiento en un número utilizando la función Number() y luego se verifica si el tipo de dato resultante es igual a 'number'. Si no es un número, se pasa a la siguiente parte de la condición.
+    // typeof string !== 'string': Esta parte verifica si la variable mensaje es de tipo 'string'. 
+    // Si cualquiera de las dos partes se evalúa como verdadera se lanza un error TypeError con el mensaje "Argumentos inválidos".
+    // Este fragmento de código asegura de que los argumentos pasados a una función sean válidos, es decir, que offset sea un número y que string sea una cadena de texto, de lo contrario se indicará un error en pantalla. 
+
+    for (let i = 0; i < mensaje.length; i++) {
+      const letraACodificar = mensaje[i].charCodeAt();/*con charCodeAt(0) se obtiene el código ASCII de la letra y lo almacena como letra codificada*/
+
+      if (letraACodificar !== 32) {
+        nuevaLetraCodificada = letraACodificar + (desplazamiento % 26);
+
+        if (nuevaLetraCodificada < 65) { /*luego si el nuevo valor de código ASCII es <65*/
+          nuevaLetraCodificada += 26; /*sumamos 26*/
+        } else if (nuevaLetraCodificada > 90) { /*si es > 90¨*/
+          nuevaLetraCodificada -= 26; /*le restamos 26*/
+        }
+      } else {
+        nuevaLetraCodificada = 32;
+      }
+
+      const nuevaLetra = String.fromCharCode(nuevaLetraCodificada); /*luego el código ASCII nuevo lo convertimos a letra, lo almacenamos como nueva letra*/
+      mensajeEncriptado += nuevaLetra;
+      console.log(mensajeEncriptado);
+
+    }
+
+    return mensajeEncriptado; /*y devolvemos ese nuevo valor*/
+  },
+
+
+  decode: (desplazamiento2, mensaje2) => {
+    let mensajeDesencriptado = "";
+    let nuevaLetraDecodificada = "";
+
+    if (typeof Number(desplazamiento2) !== 'number' || typeof mensaje2 !== 'string') {
+      throw new TypeError('Agrumentos inválidos');
+    }
+
+    for (let i = 0; i < mensaje2.length; i++) {
+      const letraADecodificar = mensaje2[i].charCodeAt();
+
+      if (letraADecodificar !== 32) {
+        nuevaLetraDecodificada = letraADecodificar - (desplazamiento2 % 26);
+
+        if (nuevaLetraDecodificada < 65) {
+          nuevaLetraDecodificada += 26;
+        } else if (nuevaLetraDecodificada > 90) {
+          nuevaLetraDecodificada -= 26;
+        }
+      } else {
+        nuevaLetraDecodificada = 32;
+      }
+
+      const nuevaLetra2 = String.fromCharCode(nuevaLetraDecodificada);
+      mensajeDesencriptado += nuevaLetra2;
+      console.log(mensajeDesencriptado);
+    }
+    return mensajeDesencriptado;
+  },
+
+};
 export default cipher;
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,59 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const btnCodificar = document.querySelector('#codificar'); /*captura el click dado por el usuario*/
+const btnDecodificar = document.querySelector('#decodificar');
+
+const originalInput = document.querySelector("#original"); /*captura el msj ingresado por el usuario*/
+const desplazamientoInput = document.querySelector("#desplazamiento"); /*captura el desplazamiento ingresado por el usuario*/
+const resultadoInput = document.querySelector("#resultado"); /*captura el resultado generado por el desplazamiento*/
+
+let mensajeOriginal = originalInput.value; /*Es un objeto que permite que el valor del msj ingresado por el usuario se asigne a una variable*/
+
+originalInput.addEventListener("input", aMayuscula, false); /*escucha un cambio en input de mensaje original y aplica la función a mayúscula*/
+
+function aMayuscula(e) { /*la función debe tener un evento (e) en su parámetro para usar el objeto target que devuelve el método del DOM que generó la ejecución de la función*/
+
+  mensajeOriginal = e.target.value; /*Permite acceder al valor de mensaje original desde dentro del controlador del evento en sí (que es la función)*/
+  mensajeOriginal = mensajeOriginal.toUpperCase(); /*cambia lo introducido por el usuario a mayúscula*/
+  /*mensajeOriginal = mensajeOriginal.replace(/[^A-Z]/, ''); /*.replace ejecuta una búsqueda por una coincidencia en una cadena y reemplaza la subcadena coincidente con una subcadena de reemplazo.*/
+
+  e.target.value = mensajeOriginal; /*renombro e.target.value como msj orig después de los cambios*/
+}
+
+btnCodificar.addEventListener("click", encripta, false); /* escucha el cambio el click y aplica la función encripta, el primer parámetro es un string con el nombre del evento que inicializa, el segundo la función a ejecutar y el tercero normalmente es falso pero no sé por qué, encontré que no captura el evento en sí, sino el burbujeo pero no lo entiendo*/
+
+function encripta() { 
+  let mensajeEncriptado = "";/*defino variable mensaje encriptado como un elemento vacío*/
+  const desplazamiento = desplazamientoInput.value ? Number(desplazamientoInput.value) : 0; /*El valor input obtenido en desplazamiento se pasa a la función Number, la función number convierte el valor a un número, si el valor es válido devuelve ese número, si el valor es inválido o está vacío Number devolverá NaN pero en este caso devuelve cero porque estoy usando un operador ternario ? : es una forma de escribir un condicional if/else en una sola línea*/
+  console.log(desplazamiento); /*se imprime en pantalla el valor válido de desplazamiento*/
+       
+  for (const letra of mensajeOriginal) { /*Aquí recorre cada letra de mensaje original usando for.. of, para cada letra llama a la función desplazamientoLetra*/
+    mensajeEncriptado += cipher.encode(desplazamiento, letra); /*para cifrarla con el desplazamiento especificado, agrega la letra cifrada a mensaje encriptado*/
+  }
+  console.log(mensajeEncriptado);/*imprime mensaje encriptado en consola*/
+  resultadoInput.value = mensajeEncriptado; /*Establece mensajeEncriptado como el mensaje grabado*/
+      
+}
+
+btnDecodificar.addEventListener("click", desencripta, false); 
+
+function desencripta() {
+  let mensajeDesencriptado = "";
+  const desplazamiento2 = desplazamientoInput.value ? Number(desplazamientoInput.value) : 0; 
+  console.log(desplazamiento2); 
+
+  for (const letra2 of mensajeOriginal) { 
+    mensajeDesencriptado += cipher.decode(desplazamiento2, letra2); 
+  }
+  console.log(mensajeDesencriptado);
+  resultadoInput.value = mensajeDesencriptado;    
+}
+
+const btnLimpiar = document.querySelector('#limpiar');
+btnLimpiar.addEventListener("click", limpiarInputs);
+
+function limpiarInputs(){
+  document.getElementById("original").value= "";
+  document.getElementById("desplazamiento").value= "";
+  document.getElementById("resultado").value= "";
+}
