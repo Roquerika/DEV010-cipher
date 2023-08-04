5653f139f4cab20dc77f6f2b820aa44fa292e366
Erika Roque <roquerika@gmail.com>
2023-08-04 11:41:02 -0400

    Nueva Corrección

--- src/cipher.js
+++ src/cipher.js
@@ -14,43 +14,43 @@ const cipher = {
     // Este fragmento de código asegura de que los argumentos pasados a una función sean válidos, es decir, que offset sea un número y que string sea una cadena de texto, de lo contrario se indicará un error en pantalla. 
 
     for (let i = 0; i < mensaje.length; i++) {
-      const letraACodificar = mensaje[i].charCodeAt();/*con charCodeAt(0) se obtiene el código ASCII de la letra y lo almacena como letra codificada*/
+      const letraACodificar = mensaje[i].charCodeAt();/*charCodeAt() cambia a ASCII la letra*/
 
       if (letraACodificar !== 32) {
         nuevaLetraCodificada = letraACodificar + (desplazamiento % 26);
 
-        if (nuevaLetraCodificada < 65) { /*luego si el nuevo valor de código ASCII es <65*/
-          nuevaLetraCodificada += 26; /*sumamos 26*/
-        } else if (nuevaLetraCodificada > 90) { /*si es > 90¨*/
-          nuevaLetraCodificada -= 26; /*le restamos 26*/
+        if (nuevaLetraCodificada < 65) { 
+          nuevaLetraCodificada += 26; 
+        } else if (nuevaLetraCodificada > 90) { 
+          nuevaLetraCodificada -= 26; 
         }
       } else {
         nuevaLetraCodificada = 32;
       }
 
-      const nuevaLetra = String.fromCharCode(nuevaLetraCodificada); /*luego el código ASCII nuevo lo convertimos a letra, lo almacenamos como nueva letra*/
+      const nuevaLetra = String.fromCharCode(nuevaLetraCodificada); 
       mensajeEncriptado += nuevaLetra;
       console.log(mensajeEncriptado);
 
     }
 
-    return mensajeEncriptado; /*y devolvemos ese nuevo valor*/
+    return mensajeEncriptado; /*return devuelve el valor de la variable indicada*/
   },
 
 
-  decode: (desplazamiento2, mensaje2) => {
+  decode: (desplazamientoDos, mensajeDos) => {
     let mensajeDesencriptado = "";
     let nuevaLetraDecodificada = "";
 
-    if (typeof Number(desplazamiento2) !== 'number' || typeof mensaje2 !== 'string') {
+    if (typeof Number(desplazamientoDos) !== 'number' || typeof mensajeDos !== 'string') {
       throw new TypeError('Agrumentos inválidos');
     }
 
-    for (let i = 0; i < mensaje2.length; i++) {
-      const letraADecodificar = mensaje2[i].charCodeAt();
+    for (let i = 0; i < mensajeDos.length; i++) {
+      const letraADecodificar = mensajeDos[i].charCodeAt();
 
       if (letraADecodificar !== 32) {
-        nuevaLetraDecodificada = letraADecodificar - (desplazamiento2 % 26);
+        nuevaLetraDecodificada = letraADecodificar - (desplazamientoDos % 26);
 
         if (nuevaLetraDecodificada < 65) {
           nuevaLetraDecodificada += 26;
@@ -61,8 +61,8 @@ const cipher = {
         nuevaLetraDecodificada = 32;
       }
 
-      const nuevaLetra2 = String.fromCharCode(nuevaLetraDecodificada);
-      mensajeDesencriptado += nuevaLetra2;
+      const nuevaLetraDos = String.fromCharCode(nuevaLetraDecodificada);
+      mensajeDesencriptado += nuevaLetraDos;
       console.log(mensajeDesencriptado);
     }
     return mensajeDesencriptado;
--- src/index.js
+++ src/index.js
@@ -1,38 +1,36 @@
 import cipher from './cipher.js';
 
-const btnCodificar = document.querySelector('#codificar'); /*captura el click dado por el usuario*/
+const btnCodificar = document.querySelector('#codificar'); /*querySelector permite capturar*/
 const btnDecodificar = document.querySelector('#decodificar');
 
-const originalInput = document.querySelector("#original"); /*captura el msj ingresado por el usuario*/
-const desplazamientoInput = document.querySelector("#desplazamiento"); /*captura el desplazamiento ingresado por el usuario*/
-const resultadoInput = document.querySelector("#resultado"); /*captura el resultado generado por el desplazamiento*/
+const originalInput = document.querySelector("#original");
+const desplazamientoInput = document.querySelector("#desplazamiento");
+const resultadoInput = document.querySelector("#resultado"); 
 
-let mensajeOriginal = originalInput.value; /*Es un objeto que permite que el valor del msj ingresado por el usuario se asigne a una variable*/
+let mensajeOriginal = originalInput.value; /*.value convierte el input en un objeto y permite asignar el valor del input a una variable*/
 
-originalInput.addEventListener("input", aMayuscula, false); /*escucha un cambio en input de mensaje original y aplica la función a mayúscula*/
+originalInput.addEventListener("input", aMayuscula, false); /*addEventListener permite ejecutar una función dada a partir de una cambio en el input o al escuchar un click*/
 
-function aMayuscula(e) { /*la función debe tener un evento (e) en su parámetro para usar el objeto target que devuelve el método del DOM que generó la ejecución de la función*/
+function aMayuscula(e) { 
 
-  mensajeOriginal = e.target.value; /*Permite acceder al valor de mensaje original desde dentro del controlador del evento en sí (que es la función)*/
-  mensajeOriginal = mensajeOriginal.toUpperCase(); /*cambia lo introducido por el usuario a mayúscula*/
-  /*mensajeOriginal = mensajeOriginal.replace(/[^A-Z]/, ''); /*.replace ejecuta una búsqueda por una coincidencia en una cadena y reemplaza la subcadena coincidente con una subcadena de reemplazo.*/
-
-  e.target.value = mensajeOriginal; /*renombro e.target.value como msj orig después de los cambios*/
+  mensajeOriginal = e.target.value; /*OJO REVISAR*/ /*Permite acceder al valor de mensaje original desde dentro del controlador del evento en sí (que es la función)*/
+  mensajeOriginal = mensajeOriginal.toUpperCase(); /*toUpperCase a mayúscula*/
+ 
+  e.target.value = mensajeOriginal; 
 }
 
-btnCodificar.addEventListener("click", encripta, false); /* escucha el cambio el click y aplica la función encripta, el primer parámetro es un string con el nombre del evento que inicializa, el segundo la función a ejecutar y el tercero normalmente es falso pero no sé por qué, encontré que no captura el evento en sí, sino el burbujeo pero no lo entiendo*/
+btnCodificar.addEventListener("click", encripta, false); 
 
 function encripta() { 
-  let mensajeEncriptado = "";/*defino variable mensaje encriptado como un elemento vacío*/
-  const desplazamiento = desplazamientoInput.value ? Number(desplazamientoInput.value) : 0; /*El valor input obtenido en desplazamiento se pasa a la función Number, la función number convierte el valor a un número, si el valor es válido devuelve ese número, si el valor es inválido o está vacío Number devolverá NaN pero en este caso devuelve cero porque estoy usando un operador ternario ? : es una forma de escribir un condicional if/else en una sola línea*/
-  console.log(desplazamiento); /*se imprime en pantalla el valor válido de desplazamiento*/
+  let mensajeEncriptado = "";
+  const desplazamiento = desplazamientoInput.value ? Number(desplazamientoInput.value) : 0; /*Es un condicional ternario*/
+  console.log(desplazamiento); 
        
-  for (const letra of mensajeOriginal) { /*Aquí recorre cada letra de mensaje original usando for.. of, para cada letra llama a la función desplazamientoLetra*/
-    mensajeEncriptado += cipher.encode(desplazamiento, letra); /*para cifrarla con el desplazamiento especificado, agrega la letra cifrada a mensaje encriptado*/
+  for (const letra of mensajeOriginal) { /*for of*/
+    mensajeEncriptado += cipher.encode(desplazamiento, letra); /*se enlaza al objeto cipher.encode*/
   }
-  console.log(mensajeEncriptado);/*imprime mensaje encriptado en consola*/
-  resultadoInput.value = mensajeEncriptado; /*Establece mensajeEncriptado como el mensaje grabado*/
-      
+  console.log(mensajeEncriptado);
+  resultadoInput.value = mensajeEncriptado; 
 }
 
 btnDecodificar.addEventListener("click", desencripta, false); 
@@ -48,6 +46,11 @@ function desencripta() {
   console.log(mensajeDesencriptado);
   resultadoInput.value = mensajeDesencriptado;    
 }
+
+//function desencripta() {
+  //const offsetTwo = desplazamientoInput.value ? Number(desplazamientoInput.value) : 0; 
+  //resultadoInput.value = cipher.decode(offsetTwo, mensajeOriginal.value); 
+//}
 
 const btnLimpiar = document.querySelector('#limpiar');
 btnLimpiar.addEventListener("click", limpiarInputs);
